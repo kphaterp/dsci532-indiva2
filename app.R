bb58c9cc8de02f16d7ae72c805d14a39b2db7bae
Kiran Phaterpekar <kphaterp@student.ubc.ca>
2022-03-08 20:25:18 -0800

    add changes to app

--- app.R
+++ app.R
@@ -4,52 +4,40 @@ library(dashHtmlComponents)
 library(dashBootstrapComponents)
 library(ggplot2)
 library(plotly)
+library(dplyr)
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
-
-msleep2 <- readr::read_csv(here::here('data', 'msleep.csv'))
+data <- read.csv("data/athlete_events_2000.csv")
 
 app$layout(
-    dbcContainer(
-        list(
-            htmlH1('Dashr heroky deployment'),
-            dccGraph(id='plot-area'),
-            htmlDiv(id='output-area'),
-            htmlBr(),
-            htmlDiv(id='output-area2'),
-            htmlBr(),
-            dccDropdown(
-                id='col-select',
-                options = msleep2 %>% colnames %>% purrr::map(function(col) list(label = col, value = col)),
-                value='bodywt')
-        )
-    )
+  dbcContainer(
+    list(
+      dccGraph(id='plot-area'),
+      dccDropdown(
+          id='col-select',
+          options = list(
+            list(label = 'Male', value = 'Male'),
+            list(label = 'Female', value = 'Female')
+          ),
+          value='Male')
+      )
+  )
 )
-
 app$callback(
     output('plot-area', 'figure'),
     list(input('col-select', 'value')),
     function(xcol) {
-        p <- ggplot(msleep2) +
-            aes(x = !!sym(xcol),
-                y = sleep_total,
-                color = vore,
-                text = name) +
-            geom_point() +
-            scale_x_log10() +
-            ggthemes::scale_color_tableau()
-        ggplotly(p) %>% layout(dragmode = 'select')
+        filtered_df <- data |> filter(Sex == xcol)
+        p <- ggplot(filtered_df) +
+            aes(x = Age) +
+            geom_histogram(fill = 'lightblue', color = 'black') +
+            ggthemes::scale_color_tableau() +
+            labs(x = "Age", y = "Number of medals received",
+                 title = "Age of athletes by the number of medals they received from 2000-2016")
+        ggplotly(p)
     }
 )
 
-app$callback(
-    list(output('output-area', 'children'),
-         output('output-area2', 'children')),
-    list(input('plot-area', 'selectedData'),
-         input('plot-area', 'hoverData')),
-    function(selected_data, hover_data) {
-        list(toString(selected_data), toString(hover_data))
-    }
-)
+
 
 app$run_server(host = '0.0.0.0')
